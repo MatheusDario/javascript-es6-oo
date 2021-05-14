ea70e9ac3ca8ab7e0bb2d532bfca536748518638
Matheus Dario <dario.matheus97@gmail.com>
2021-05-14 10:10:03 -0300

    adicionando dinamicamente os valores no localStorage

--- app1_despesas/app.js
+++ app1_despesas/app.js
@@ -9,6 +9,32 @@ class Despesa {
     }
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return (parseInt(proximoId) +1)
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id',id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
 
    let ano = document.getElementById('ano')
@@ -20,5 +46,6 @@ function cadastrarDespesa() {
 
    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-   console.log(despesa)
+   bd.gravar(despesa)
 }
+
